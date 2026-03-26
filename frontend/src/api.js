82f332f6dab457a621525a05bfd3782eb5b19aa7
Hibha <hibhasmehrok@gmail.com>
2026-03-26 18:07:05 +0530

    Updated api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
-const BASE = process.env.REACT_APP_BACKEND_URL
+const BASE = process.env.REACT_APP_BACKEND_URL 
   ? `${process.env.REACT_APP_BACKEND_URL}/api`
-  : 'http://localhost:5000/api';
+  : '/api';
 
 const getToken = () => localStorage.getItem('token');
 
